0b809307530e400f3017a0136289776bcd01d020
Yashpal Singh <yashpalsingh9rx@outlook.com>
2025-01-26 23:59:47 +0530

    sd

--- src/advSQL/1select.ts
+++ src/advSQL/1select.ts
@@ -2,7 +2,7 @@ import { executeQuery } from "../utils";
 
 export async function selectNow() {
     const result = await executeQuery(async (client) => {
-        return await client.query('');
+        return await client.query('SELECT NOW()');
     });
     // console.log(result);
     console.log('🎉 Done 🎉');
